75c5f99ecf58345ef5fe517fa3e365bdfa4942aa
perry <perry2008084@gmail.com>
2016-03-20 07:50:09 -0800

    make the server module

--- Nodejs/pictureshow/server.js
+++ Nodejs/pictureshow/server.js
@@ -1,6 +1,6 @@
 var http = require("http");
 
-
+function start() {
 function onRequest(request, response) {
 	console.log("Request received");
 	response.writeHead(200, {"Content-Type": "text/plain"});
@@ -11,3 +11,6 @@ function onRequest(request, response) {
 http.createServer(onRequest).listen(8888);
 
 console.log("Server has started.");
+}
+
+exports.start = start;
